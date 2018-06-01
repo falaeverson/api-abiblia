e6752a36f6827771610d6ed4f0b6d563351fb9f5
Everson <falaeverson@gmail.com>
2018-06-01 00:38:54 -0300

    1º Commit com fontes do projeto

---
+++ src/main/groovy/com/abiblia/abiblia/AbibliaApplication.groovy
@@ -0,0 +1,12 @@
+package com.abiblia.abiblia
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class AbibliaApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run AbibliaApplication, args
+	}
+}
