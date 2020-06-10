0864cbae9fe6012275d3a98ec09ef7936ef05942
Everson Teixeira <tx.everson@gmail.com>
2020-06-10 20:46:55 -0300

    Remoção do prefixo 'api' dos endereços

--- src/main/java/org/api/abiblia/util/ConstantesRest.java
+++ src/main/java/org/api/abiblia/util/ConstantesRest.java
@@ -3,11 +3,9 @@ package org.api.abiblia.util;
 public class ConstantesRest {
 
 	public static final String API_VERSION = "1.0";
-	public static final String PATH_VERSION = "/v1";
-	public static final String PATH_API =  "/api";
-	public static final String PATH_LIVRO = PATH_API + "/livros";
-	public static final String PATH_VERSAO = PATH_API + "/versoes";
-	public static final String PATH_VERSICULO = PATH_API + "/versiculos";
+	public static final String PATH_LIVRO = "/livros";
+	public static final String PATH_VERSAO = "/versoes";
+	public static final String PATH_VERSICULO = "/versiculos";
 	
 	
 	
